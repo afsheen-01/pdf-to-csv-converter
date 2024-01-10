b3b5d19311eea51c90fc3d79916e1e37452474af
FCI-2143 <fci-2143@Apples-MacBook-Pro.local>
2024-01-11 01:45:04 +0530

    conversion of 1st page of RBL data correctly

--- source/pdf-to-csv.py
+++ source/pdf-to-csv.py
@@ -3,9 +3,6 @@ import PyPDF2
 import re
 import csv
 
-# Read PDF into a DataFrame
-# rblPdfPage1 = tabula.read_pdf("../../Documents/AccountStatement01-10-2023-to-04-01-2024.pdf", pages='5')
-
 def clean_up_rbl_data(account_data):
     # Extracting relevant account information using regular expressions
     opening_balance_match = re.search(r'Opening\s+Balance₹\s+([\d,]+\.\d+)', account_data)
@@ -14,7 +11,6 @@ def clean_up_rbl_data(account_data):
     count_of_debit_match = re.search(r'Count\s+Of\s+Debit(\d+)', account_data)
     count_of_credit_match = re.search(r'Count\s+Of\s+Credit(\d+)', account_data)
     lien_amt_match = re.search(r'Lien\s+Amt₹\s+([\d,]+\.\d+)', account_data)
-
 
     # Creating a dictionary with the extracted information
     cleaned_data = {
@@ -31,31 +27,32 @@ def clean_up_rbl_data(account_data):
 
 def extract_table_from_pdf(pdf_path, page_number=1):
     # Specify the area containing the table using the 'area' parameter
-    table_area = [502.56, 33.84, 685.44, 560.88]  # Adjust these values based on your PDF layout
+    table_area = [514.08, 33.84, 685.44, 560.88]  # Adjust these values based on your PDF layout
 
     # Read the PDF and extract the table
-    table_df = tabula.read_pdf(pdf_path, pages=page_number, multiple_tables=True, pandas_options={'header': None}, area=table_area)
+    table_df = tabula.read_pdf(pdf_path, pages=page_number, multiple_tables=True, area=table_area)
 
     return table_df
 
 def extract_rbl_data_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
 
         # Assuming you want to extract text data from the first page
         account_details_page = pdf_reader.pages[4]
-        account_details_data = account_details_page.extract_text()
-
+        account_details_data = account_details_page.extractText()
 
         # Table out of RBL PDF
         records_data = extract_table_from_pdf(pdf_path)
 
-        print(records_data)
+        # Save the table data to a CSV file
+        if records_data:
+            print(records_data[0])
+            records_data[0].to_csv("../rbl_table_data.csv", index=False)
 
         clean_data = clean_up_rbl_data(account_details_data)
 
         return clean_data
-
 
 pdf_path = "../../Documents/AccountStatement01-10-2023-to-04-01-2024.pdf"
 data_from_pdf = extract_rbl_data_from_pdf(pdf_path)
